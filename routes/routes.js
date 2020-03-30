f7a1534f0832a30da19d8682ee425b7730b3238b
jimmy nagles <jimmynagles@gmail.com>
2020-03-30 11:55:49 -0700

    b

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const scrape = require("../script/scrape");
 var axios = require("axios");
 var cheerio = require("cheerio");
-const db = require("../models")
+const db = require("../models");
 
 
 router.get('/', function (req, res) {
