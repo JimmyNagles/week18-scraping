c0f7ad4095710d09f28f24c6ab1d2b464c8211a2
jimmy nagles <jimmynagles@gmail.com>
2020-03-30 11:27:46 -0700

    a

--- models/Article.js
+++ models/Article.js
@@ -17,6 +17,7 @@ var ArticleSchema = new Schema({
     required: true
   },
   image: {
+
     type: String,
     required: true
   },
@@ -29,13 +30,8 @@ var ArticleSchema = new Schema({
       type: String,
       required: true
     },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
-  note: {
-    type: Schema.Types.ObjectId,
-    ref: "Note"
-  }
+
+    
 });
 
 // This creates our model from the above schema, using mongoose's model method
